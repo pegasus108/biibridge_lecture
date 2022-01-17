1bafbbe9f2b39dc895944d2fbfad38eca95927d4
nina2170 <75292835+nina2170@users.noreply.github.com>
2022-01-17 14:12:05 +0900

    footer修正

--- laravel/resources/views/components/footer.blade.php
+++ laravel/resources/views/components/footer.blade.php
@@ -2,15 +2,13 @@
     <div id="Ft">
         <div class="ft-navigation">
             <h1><img src="{{ asset('image/common/logo_white.png') }}" alt=""/></h1>
-            {{-- <address>〒100-0014 東京都千代田区永田町1丁目7-1</address> --}}
-            @inject('officeaddress','App\Repositories\CompanyRepository')
-            {{$officeaddress->getOfficeAddress()->value}}
+            <address>〒100-0014 東京都千代田区永田町1丁目7-1</address>
             <nav>
                 <ul>
-                    <li><a href="{{ route('company.recruit') }}">採用情報</a></li>
-                    <li><a href="{{ route('company.privacy') }}">プライバシーポリシー</a></li>
-                    <li><a href="{{ route('company.about') }}">会社概要</a></li>
-                    <li><a href="{{ route('info.contact') }}">お問い合わせ</a></li>
+                    <li><a href="#">採用情報</a></li>
+                    <li><a href="#">プライバシーポリシー</a></li>
+                    <li><a href="#">会社概要</a></li>
+                    <li><a href="#">お問い合わせ</a></li>
                 </ul>
             </nav>
         </div>
